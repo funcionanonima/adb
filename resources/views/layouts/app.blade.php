5954f9b557aa62c21f13b4f38a8c5350e9e344aa
Bambooh <cdgaviria40@misena.edu.co>
2019-08-15 20:20:55 -0500

    se cambian los colores warning y primary para estilos corporativos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,6 +59,9 @@
                                 <a class="nav-link" href="{{ route('areas') }}">{{ __('Areas') }}</a>
                             </li>
                         @endrole
+                        <li class="nav-item">
+                                <a class="nav-link" href="{{ route('home') }}">{{ __('Index') }}</a>
+                            </li>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }} <span class="caret"></span>
