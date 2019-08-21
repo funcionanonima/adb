10628b913e78617d540c67b6f48c2aac4d74cbe7
Bambooh <cdgaviria40@misena.edu.co>
2019-08-21 00:42:41 -0500

    se revierte pgsql

--- config/database.php
+++ config/database.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Str;
-$DATABASE_URL = parse_url('postgres://yjritdgaycvgks:9486b208d3e4dc7fce7f925690cf76b142bf54842d83e7d0e400566e2c9254fb@ec2-23-23-173-30.compute-1.amazonaws.com:5432/d23j82levr9726
-');
 
 return [
 
@@ -17,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => $DATABASE_URL['host'],
-            'port' => $DATABASE_URL['port'],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL['user'],
-            'password' => $DATABASE_URL['pass'],
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
